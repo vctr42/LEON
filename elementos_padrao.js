a64a6a67a4fa45a6cde93a44352f75657656bfcf
vctr42 <victorgois52@hotmail.com>
2024-05-29 18:31:25 -0300

    elementos padrao

---
+++ elementos_padrao.js
@@ -0,0 +1,14 @@
+// Link header
+fetch('header.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+});
+
+//link footer
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer').innerHTML = data;
+});
+
